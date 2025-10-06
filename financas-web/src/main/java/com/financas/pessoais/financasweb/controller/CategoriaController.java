3847b6fe4723123c5304490636e4d6d9fcb51cef
paulo.velosa <paulovelosa.dev2025@gmail.com>
2025-10-06 12:33:19 -0300

    Ajusta endpoint para /api/categorias

--- financas-web/src/main/java/com/financas/pessoais/financasweb/controller/CategoriaController.java
+++ financas-web/src/main/java/com/financas/pessoais/financasweb/controller/CategoriaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categorias")
+@RequestMapping("/api/categorias")
 @CrossOrigin(origins = "*")
 public class CategoriaController {
 
