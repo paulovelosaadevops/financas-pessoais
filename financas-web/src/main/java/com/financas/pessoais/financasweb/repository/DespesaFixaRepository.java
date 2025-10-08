7728a1bb749f6224b4bce3aac584f18bd2eda9f4
paulo.velosa <paulovelosa.dev2025@gmail.com>
2025-10-08 09:51:22 -0300

    Correções na Dashboard

--- financas-web/src/main/java/com/financas/pessoais/financasweb/repository/DespesaFixaRepository.java
+++ financas-web/src/main/java/com/financas/pessoais/financasweb/repository/DespesaFixaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DespesaFixaRepository extends JpaRepository<DespesaFixa, Long> {
